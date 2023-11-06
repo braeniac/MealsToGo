5edd2dfef019ec189c5f247b6f8ecdb7a20bc3e4
Maninder Singh <manny.singh.live@gmail.com>
2023-11-06 07:07:45 -0500

    added star rating to restaurant

--- src/features/restaurants/components/restaurants-info-card.components.js
+++ src/features/restaurants/components/restaurants-info-card.components.js
@@ -1,6 +1,8 @@
 import React from 'react'; 
 import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
+import { SvgXml } from 'react-native-svg';
+import star from '../../../../assets/star';
 
 const Title = styled.Text`
     font-family: ${(props) => props.theme.fonts.heading};
@@ -27,6 +29,12 @@ const Info = styled.View`
     padding: ${(props) => props.theme.space[3]};
 `
 
+const Rating = styled.View`
+    flex-direction: row;
+    padding-top: ${(props) => props.theme.space[2]}; 
+    padding-bottom: ${(props) => props.theme.space[2]}; 
+`
+
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
@@ -42,11 +50,20 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         isClosedTemporarily,
     } = restaurant; 
 
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
+
     return(
         <RestaurantCard elevation={5}>
             <RestaurantCardCover key={name} source={{ uri : photos[0] }} />
             <Info>
                 <Title>{name}</Title>
+                <Rating>
+                    {
+                        ratingArray.map(() => (
+                            <SvgXml xml={star} width={20} height={20} />
+                        ))
+                    }
+                </Rating>
                 <Address>{address}</Address>
             </Info>
         </RestaurantCard>
